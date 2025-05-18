40049f795e9690c081dbe7d9e8e8d9224ea8f00a
Franciso Martin Serafini <92947974+Supertact@users.noreply.github.com>
2025-05-17 21:24:09 -0300

    Creacion de dispositivos.py

---
+++ dispositivos.py
@@ -0,0 +1,12 @@
+dispositivos = {
+    "tv": {
+        "tipo": "televisor",
+        "estado": "apagado",
+        "ubicacion": "dormitorio"
+    },
+    "musica": {
+        "tipo": "parlante",
+        "estado": "apagado",
+        "ubicacion": "dormitorio"
+    },
+}
